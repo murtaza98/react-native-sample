4c9f386a978f7e1fdf34a3c11ec64fbd130c8c9b
murtaza98 <murtaza98.personal@gmail.com>
2024-04-01 16:00:44 +0530

    add test

---
+++ App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import App from './App';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(<App />);
+    
+    // Assert that the text "Your app is working!" is rendered
+    expect(getByText('Your app is working!')).toBeTruthy();
+    
+    // Assert that the text "Next, open up App.js to start working on your app!" is rendered
+    expect(getByText('Next, open up App.js to start working on your app!')).toBeTruthy();
+  });
+});
