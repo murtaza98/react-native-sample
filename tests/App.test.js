e5a0411493ea2a175bf2f1e1d6b3042a21870127
murtaza98 <murtaza98.personal@gmail.com>
2024-04-30 10:38:42 +0530

    fix non-zero score warning

--- tests/App.test.js
+++ tests/App.test.js
@@ -3,20 +3,19 @@ import { render } from '@testing-library/react-native';
 import App from '../App';
 
 describe('App', () => {
+  // Note: This test is expected to fail by default
   it('renders correctly', () => {
     const { getByText } = render(<App />);
     
-    // Assert that the text "Your app is working!" is rendered
-    expect(getByText('Your app is working!')).toBeTruthy();
+    // Fix this test by replacing the following assertion
+    expect(getByText('Your app is not working!')).toBeTruthy();
     
-    // Assert that the text "Next, open up App.js to start working on your app!" is rendered
     expect(getByText('Next, open up App.js to start working on your app!')).toBeTruthy();
   });
   // Note: This test is expected to fail by default
   it('should display a button', () => {
     const { getByText } = render(<App />);
     
-    // Assert that the button is rendered. 
     expect(getByText('Press me!')).toBeTruthy();
   });
 });
